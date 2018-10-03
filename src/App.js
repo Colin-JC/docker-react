ff23601dcd9c58b9dd0f2f5495fc34d68400a11f
Colin-JC <colin.jc@live.fr>
2018-10-03 14:13:56 +0900

    text change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Changed on feature branch a second time
+          Changing the text for test
         </p>
       </div>
     );
